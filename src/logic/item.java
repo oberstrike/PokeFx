aab0a987f32449598625fe181c7116e076948146
Unescience <38902530+Unescience@users.noreply.github.com>
2018-05-02 12:54:58 +0200

    items

---
+++ src/logic/item.java
@@ -0,0 +1,57 @@
+package logic;
+
+public class item {
+	
+	private int id;
+	private String name;
+	private String effect;
+	private String entity;
+	private double rate;
+	
+	// Konstruktor
+	
+	public item(int id, String name, String effect, String entity, double rate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.effect = effect;
+		this.entity = entity;
+		this.rate = rate;
+	}
+	
+	//Getter + Setter
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEffect() {
+		return effect;
+	}
+	public void setEffect(String effect) {
+		this.effect = effect;
+	}
+	public String getEntity() {
+		return entity;
+	}
+	public void setEntity(String entity) {
+		this.entity = entity;
+	}
+	public double getRate() {
+		return rate;
+	}
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+	
+	
+
+}
