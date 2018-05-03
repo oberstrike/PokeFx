836da98a8d93f1fecebf2c213b758dbcb0904dd4
oberstrike <markus.juergens@gmx.de>
2018-05-03 14:41:36 +0200

    Updated

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -5,20 +5,15 @@ public class Player {
 	private String name;
 	private double x;
 	private double y;
-<<<<<<< HEAD
-	private Pokemon pokemon;
 	private Field field;
 
-	public Player() {
 
-=======
 	private Pokemon[] pokemon = new Pokemon[3];
-	private Item[] item = new Item[5];
+	//private Item[] item = new Item[5];
 	private float motivation;
 	
 	public Player(){
 		
->>>>>>> d7c928c7349a7a287304ba5eb387d7dccd917c1b
 	}
 
 	public double getX() {
@@ -40,13 +35,20 @@ public class Player {
 	public Player(double x, double y) {
 
 	}
-<<<<<<< HEAD
 
-	public Pokemon getPokemon() {
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Pokemon[] getPokemon() {
 		return pokemon;
 	}
 
-	public void setPokemon(Pokemon pokemon) {
+	public void setPokemon(Pokemon[] pokemon) {
 		this.pokemon = pokemon;
 	}
 
@@ -58,7 +60,5 @@ public class Player {
 		this.field = field;
 	}
 
-=======
-	
->>>>>>> d7c928c7349a7a287304ba5eb387d7dccd917c1b
+
 }
